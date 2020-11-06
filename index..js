6d57981e291d40876577e9532da4174f86347ce8
ArlayshaO <71731654+ArlayshaO@users.noreply.github.com>
2020-11-05 21:13:07 -0600

    test

---
+++ index..js
@@ -0,0 +1,28 @@
+
+
+    //arrays-data structure
+    let arr = [];
+    let obj = {};
+
+
+//bank account array 
+Let bankAccounts = [
+//pin/balance =objects inside the array
+
+{
+    pin: 1234,
+    balance:100.00
+},
+
+
+{
+    pin: 2234,
+    balance: 150.00
+}
+
+]
+
+console.log( bankAccounts[0] );
+comsole.log( bankAccounts[this.pin])
+
+
